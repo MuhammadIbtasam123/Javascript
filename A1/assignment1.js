1fd06c35080dc42abc0d98833f707779d8a60d4e
Muhammad ibtasam <its.ibtasamqureshi123@gmail.com>
2024-02-20 23:14:19 +0500

    Changes to Assignemnt

--- A1/assignment1.js
+++ A1/assignment1.js
@@ -48,28 +48,124 @@
 
 /* ************************** Prime numbers ************************** */
 
-function checkPrimeNumbers(startingNumber, endingNumber) {
-  let primeNumbers = [];
-  for (let i = startingNumber; i <= endingNumber; i++) {
-    let isPrime = true;
-    if (i === 1 || i === 0) {
-      isPrime = false;
-    } else if (i > 1) {
-      for (let j = 2; j < i; j++) {
-        if (i % j === 0) {
-          isPrime = false;
-          break;
-        }
-      }
-    }
-    if (isPrime) {
-      primeNumbers.push(i);
-    }
+// function checkPrimeNumbers(startingNumber, endingNumber) {
+//   let primeNumbers = [];
+//   for (let i = startingNumber; i <= endingNumber; i++) {
+//     let isPrime = true;
+//     if (i === 1 || i === 0) {
+//       isPrime = false;
+//     } else if (i > 1) {
+//       for (let j = 2; j < i; j++) {
+//         if (i % j === 0) {
+//           isPrime = false;
+//           break;
+//         }
+//       }
+//     }
+//     if (isPrime) {
+//       primeNumbers.push(i);
+//     }
+//   }
+//   return primeNumbers;
+// }
+
+// let startingNumber = Number(prompt("Enter starting number"));
+// let endingNumber = Number(prompt("Enter ending number"));
+// let PrimeNumberArray = checkPrimeNumbers(startingNumber, endingNumber);
+// console.log(PrimeNumberArray);
+
+// **************** Question 3 ****************
+// JS Class + objects)
+
+class basketProto {
+  constructor(value) {
+    this.amount = value;
   }
-  return primeNumbers;
+
+  addToBasket(amountToAdd) {
+    this.amount += amountToAdd;
+  }
+
+  pay() {
+    console.log(`${this.amount} has been paid`);
+  }
+
+  clearBasket() {
+    this.amount = 0;
+  }
 }
 
-let startingNumber = Number(prompt("Enter starting number"));
-let endingNumber = Number(prompt("Enter ending number"));
-let PrimeNumberArray = checkPrimeNumbers(startingNumber, endingNumber);
-console.log(PrimeNumberArray);
+let obj1 = new myBasket(0);
+console.log(obj1);
+console.log(typeof obj1);
+
+let obj = new basketProto(0);
+obj.addToBasket(2);
+obj.addToBasket(4);
+obj.pay();
+obj.clearBasket();
+obj.pay();
+
+// **************** Question 4 ****************
+
+class myBasket extends basketProto {
+  constructor(value) {
+    super(value);
+  }
+}
+
+// **************** Question 5 ****************
+
+// **************** Question 6 ****************
+
+// const movies = [
+//   {
+//     name: "The Shawshank Redemption",
+//     director: "Frank Darabont",
+//     imdbRating: 9.3,
+//   },
+//   { name: "The Green Mile", director: "Frank Darabont", imdbRating: 8.6 },
+//   { name: "The Mist", director: "Frank Darabont", imdbRating: 7.1 },
+//   { name: "The Godfather", director: "Francis Ford Coppola", imdbRating: 9.2 },
+//   {
+//     name: "The Godfather Part II",
+//     director: "Francis Ford Coppola",
+//     imdbRating: 9.0,
+//   },
+//   { name: "Apocalypse Now", director: "Francis Ford Coppola", imdbRating: 8.4 },
+//   { name: "The Dark Knight", director: "Christopher Nolan", imdbRating: 9.0 },
+//   { name: "Inception", director: "Christopher Nolan", imdbRating: 8.8 },
+//   { name: "Interstellar", director: "Christopher Nolan", imdbRating: 8.6 },
+//   {
+//     name: "The Lord of the Rings: The Fellowship of the Ring",
+//     director: "Peter Jackson",
+//     imdbRating: 8.8,
+//   },
+//   {
+//     name: "The Lord of the Rings: The Two Towers",
+//     director: "Peter Jackson",
+//     imdbRating: 8.7,
+//   },
+//   {
+//     name: "The Lord of the Rings: The Return of the King",
+//     director: "Peter Jackson",
+//     imdbRating: 8.9,
+//   },
+// ];
+
+// // let nameOfDirector = prompt("Enter name of director").trim().toLowerCase();
+// let nameOfDirector = "Peter Jackson";
+
+// const filteredMoviesOfSpecificDirector = movies.filter(
+//   (element) => element.director === nameOfDirector
+// );
+// console.log(filteredMoviesOfSpecificDirector);
+// const SumRating = filteredMoviesOfSpecificDirector.reduce(
+//   (sum, element) => (sum += element.imdbRating),
+//   0
+// );
+// console.log(SumRating);
+// const avgRating = (SumRating / filteredMoviesOfSpecificDirector.length).toFixed(
+//   1
+// );
+// console.log(avgRating);
