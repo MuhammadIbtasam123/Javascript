ef8fdb4a93c5037d0aae4357c846cb90efa9d3d4
Muhammad ibtasam <its.ibtasamqureshi123@gmail.com>
2024-02-24 18:29:38 +0500

    global and local scope

--- A1/Functions/GlobalLocalScope.js
+++ A1/Functions/GlobalLocalScope.js
@@ -17,22 +17,22 @@ KEY POINT:
   console.log(b);
   console.log(c);
 }
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 // here I initailased/declare variable let, const, var outside {}.
 // let const var can be accessible inside {}.
-var a = 10;
-let b = 20;
-const c = 30;
-console.log(a);
-console.log(b);
-console.log(c);
+var a1 = 10;
+let b1 = 20;
+const c1 = 30;
+console.log(a1);
+console.log(b1);
+console.log(c1);
 {
-  console.log(a);
-  console.log(b);
-  console.log(c);
+  console.log(a1);
+  console.log(b1);
+  console.log(c1);
 }
 
 // here I initailased/declare variable let, const, var outside as well inside {}.
@@ -64,3 +64,75 @@ console.log(c);
 // 110
 // 20
 // 30
+
+/* SCOPE LEVEL/NESTED  AND HOISTING AND CLOSURES 
+1- Child can access parent varibale or block can access global varibales.
+*/
+
+// Nested Scope
+function one() {
+  const username = "ibtasam";
+  function two() {
+    const app = "notarynow";
+    console.log(username);
+  }
+  //   console.log(app);
+  two();
+}
+// two(); cannot called outside the one function scope.
+one();
+// Issue:
+// app dosent run as child can access parent but parent dosent access chaild variables.
+
+// MErged Example:
+
+if (true) {
+  const username = "hitesh";
+  if (username === "hitesh") {
+    const website = " youtube";
+    console.log(username + website); // run
+  }
+  // console.log(website); // error
+}
+
+// console.log(username); /error
+
+// function declaration and calling:
+
+function AddOne(value) {
+  return value + 1;
+}
+
+console.log(AddOne(10)); // 11
+
+const AddTwo = function (value) {
+  return value + 2;
+};
+
+console.log(AddTwo(10)); // 12
+
+// ISSUE:
+// what if we call function before declaration.
+
+console.log(AddOne(10)); // 11
+function Add1(value) {
+  return value + 1;
+}
+
+console.log(AddTwo(10)); // Error
+const Add2 = function (value) {
+  return value + 2;
+};
+
+/* What is hoisting and execution context */
+
+// Hoisting:
+// 1- Hoisting is a mechanism where variables and function declarations are moved to the top of their containing scope during the compile phase.
+// 2- Only the declaration is hoisted not the initialisation.
+// 3- var hoisted but let and const not hoisted.
+// 4- Function declaration hoisted but function expression not hoisted.
+
+// Execution Context:
+// 1- Global Execution Context.
+// 2- Functional Execution Context.
+// 3- Eval Execution Context.
